ea0f807738a21c3452d498e451672a2f9dd0cf2d
Kamylle Victoria Santos <sharmankamylle@gmail.com>
2023-08-30 21:47:50 -0300

    correcao ex7 estruturas condicionais em c

--- estruturas_condicionais/ex03/main.c
+++ estruturas_condicionais/ex03/main.c
@@ -8,20 +8,23 @@ int main() {
 	int a, b ,c, delta, raiz1, raiz2;
 	printf("Informe o valor de a: ");
 	scanf("%d" ,&a);
-	printf("Informe o valor de b: ");
-	scanf("%d" ,&b);
-	printf("Informe o valor de c: ");
-	scanf("%d" ,&c);
-	delta = b*b - 4*a*c;
-	printf("The value of delta is: %d. " ,delta);
-	if (delta < 0){
-		printf("\nelta is not a valid integrer number, try again!");
+	if (a == 0){
+		printf("Operacao invalida, tente novamente mais tarde.");
 	}
 	else{
-		printf("\nDelta is a valid integrer number!");
+		printf("Informe o valor de b:");
+		scanf("%d" ,&b);
+		printf("Informe o valor de c: ");
+		scanf("%d" ,&c);
+		delta = b*b - 4*a*c;
+		if(delta == 0){
+			raiz1 = -b / (2*a);
+			printf("As raizes de um delta igual a zero sao iguais.\nRaiz 1:%d \nRaiz 2: %d.", raiz1, raiz1);
+		}
+		else{
+			raiz1 = -b + sqrt(delta)/ (2*a);
+			raiz2 = -b - sqrt(delta)/ (2*a);
+			printf("As raizes do delta sao dadas por: \nRaiz 1: %d \nRaiz2: %d." ,raiz1, raiz2);
+		}
 	}
-	raiz1 = (-b + sqrt(delta))/(2 * a);
-	raiz2 = (-b - sqrt(delta))/ (2 * a);
-	printf("%d and %d" ,raiz1, raiz2);
-
 }
