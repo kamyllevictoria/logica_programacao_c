2b79c704aa1bdbdd58fad3ba57b14d133609c958
Kamylle Victoria Santos <sharmankamylle@gmail.com>
2023-11-04 16:36:10 -0300

    ex04 strings em arrays

---
+++ repeticoes_for_arrays_7/ex04/main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char frase[100];
+    int i, tam;
+    
+    printf("\nDigite uma frase e a veja invertida: "); 
+    fflush(stdin); 
+    gets(frase);
+    tam = strlen(frase); 
+    
+    char inv[tam + 1]; 
+    //criamos novo array para frase invertida
+	//usamos o tam + 1 para paramos no tamanho 20, e nao na posição 20, pois a posição 0 é contada, logo, teriamos o tamanho de 21 e nao 20 
+	//para a reversão, adicionamos o valor de i e regredimos o valor de j;
+    int j = tam - 1;
+    
+    for (i = 0; i < tam; i++, j--) {
+        inv[j] = frase[i];
+    }
+    inv[tam] = '\0';
+    
+    printf("\nA frase invertida e dada por: %s", inv);
+    
+    return 0;
+}
+
